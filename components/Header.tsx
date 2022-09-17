0854d63ca4441c9f6fabb9c6ba15549e46e03ab2
Cooper Saye <coopersaye@gmail.com>
2022-09-16 23:39:00 -0400

    Construct header content.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+
 import styles from '../styles/components/Header.module.scss';
 
 export default function Header() {
   return (
     <div className={styles.container}>
+      <a href="#home">
+        <Image
+          src="/img/logo.svg"
+          width="64"
+          height="64"
+          alt="logo"
+        />
+      </a>
+      <h1>Cooper Saye</h1>
+      <span style={{ flexGrow: 1 }} />
+      <a href="#home">Home</a>
+      <a href="#skillset">Skillset</a>
+      <a href="#projects">Projects</a>
     </div>
   );
 }
