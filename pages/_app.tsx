facf7318206dcca478021806a922197628c6c50b
Cooper Saye <coopersaye@gmail.com>
2022-09-16 23:28:51 -0400

    Link favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,10 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Cooper Saye</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
