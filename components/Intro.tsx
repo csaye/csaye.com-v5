e1368d0bc8c2e20bd57cf68a1bf6b1e0a77157b6
Cooper Saye <coopersaye@gmail.com>
2022-09-17 23:46:38 -0400

    Construct intro section.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,17 @@
+import { Code, EmojiEvents, School } from '@mui/icons-material';
 import styles from '../styles/components/Intro.module.scss';
 
 export default function Intro() {
   return (
     <div className={styles.container}>
+      <div className={styles.hello}>
+        <h1>Hi!<br />I&apos;m Cooper Saye.</h1>
+        <p><Code />Computer Science Student</p>
+        <p><School />University of Michigan</p>
+        <p><EmojiEvents />Congressional App Challenge Winner</p>
+      </div>
+      <div className={styles.latest}>
+      </div>
     </div>
   );
 }
