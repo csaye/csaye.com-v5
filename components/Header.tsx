780e44892bb4094986634f57625767dbe316bde0
Cooper Saye <coopersaye@gmail.com>
2022-09-17 23:04:41 -0400

    Update header layout.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,15 @@
-import Image from 'next/image';
-
 import styles from '../styles/components/Header.module.scss';
 
 export default function Header() {
   return (
     <div className={styles.container}>
-      <a className={styles.logo} href="#home">
-        <Image
-          src="/img/logo.svg"
-          width="64"
-          height="64"
-          alt="logo"
-        />
-      </a>
       <h1>Cooper Saye</h1>
       <span style={{ flexGrow: 1 }} />
-      <a className={styles.link} href="#home">Home</a>
-      <a className={styles.link} href="#skillset">Skillset</a>
-      <a className={styles.link} href="#projects">Projects</a>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#skillset">Skillset</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
     </div>
   );
 }
