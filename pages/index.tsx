88e46bd987ebf4f3c57c249fa54a3998dd873b57
Cooper Saye <coopersaye@gmail.com>
2022-09-18 23:36:41 -0400

    Construct index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
+import Header from '../components/Header';
+import Intro from '../components/Intro';
 import styles from '../styles/pages/Index.module.scss';
 
 export default function Index() {
+
+  function introScroll() {
+  }
+
   return (
     <div className={styles.container}>
+      <Header />
+      <Intro introScroll={introScroll} />
     </div>
   );
 }
