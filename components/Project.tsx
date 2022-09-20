837999a0b80b9f80f5302b0cffd3a369ccbe3b43
Cooper Saye <coopersaye@gmail.com>
2022-09-19 23:49:01 -0400

    Update project props.

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image';
+import { Project as ProjectType } from '../data/projects';
 import styles from '../styles/components/Project.module.scss';
 
-export default function Project() {
+type Props = {
+  project: ProjectType;
+};
+
+export default function Project(props: Props) {
+  const { project } = props;
+  const { title, about, image } = project;
+
   return (
     <div className={styles.container}>
     </div>
