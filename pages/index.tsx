67638f6b1c2be979dcbdf406c09c9c96512138c8
Cooper Saye <coopersaye@gmail.com>
2022-09-19 23:31:43 -0400

    Implement intro scroll ref.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
+import { useRef } from 'react';
 import Header from '../components/Header';
 import Intro from '../components/Intro';
 import styles from '../styles/pages/Index.module.scss';
 
 export default function Index() {
+  const scrollRef = useRef<HTMLSpanElement>(null);
 
+  // scrolls user to first section
   function introScroll() {
+    scrollRef.current?.scrollIntoView();
   }
 
   return (
     <div className={styles.container}>
       <Header />
       <Intro introScroll={introScroll} />
+      <span ref={scrollRef} />
     </div>
   );
 }
