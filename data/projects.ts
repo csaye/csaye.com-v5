7befc01138d0b26c35c82336104c167a7bdec05e
Cooper Saye <coopersaye@gmail.com>
2022-09-19 23:42:10 -0400

    Construct dummy project data.

--- data/projects.ts
+++ data/projects.ts
@@ -3,3 +3,36 @@ type Project = {
   about: string;
   image: string;
 };
+
+export const projects = [
+  {
+    title: 'Bridge',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'bridge.png'
+  },
+  {
+    title: 'CodeCreatively',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'codecreatively.png'
+  },
+  {
+    title: 'Deserted',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'deserted.png'
+  },
+  {
+    title: 'Devcomms',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'devcomms.png'
+  },
+  {
+    title: 'Megaphone',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'megaphone.png'
+  },
+  {
+    title: 'Wick Page',
+    about: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
+    image: 'wickpage.png'
+  }
+];
