9ff989ce5d3eb6cf33e1c1f5775e55d8cafe0e98
Cooper Saye <coopersaye@gmail.com>
2022-09-19 23:48:50 -0400

    Map projects to components.

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,18 @@
+import { projects } from '../data/projects';
 import styles from '../styles/components/Projects.module.scss';
+import Project from './Project';
 
 export default function Projects() {
   return (
     <div className={styles.container}>
+      {
+        projects.map((project, i) =>
+          <Project
+            project={project}
+            key={i}
+          />
+        )
+      }
     </div>
   );
 }
