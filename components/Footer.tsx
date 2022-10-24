d9343b3952a94e2f4bae870ca912540889682479
Cooper Saye <coopersaye@gmail.com>
2022-10-23 20:57:36 -0400

    Add footer content.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/components/Footer.module.scss';
 export default function Footer() {
   return (
     <div className={styles.container}>
+      <p>&copy; Cooper Saye {new Date().getFullYear()}</p>
     </div>
   );
 }
