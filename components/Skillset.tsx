46e672b3c1eebb170d0d131fe17856fd555d3931
Cooper Saye <coopersaye@gmail.com>
2022-10-23 23:54:43 -0400

    Construct skillset section.

--- components/Skillset.tsx
+++ components/Skillset.tsx
@@ -1,8 +1,18 @@
+import { skillset } from '../data/skillset';
 import styles from '../styles/components/Skillset.module.scss';
+import Section from './Section';
 
 export default function Skillset() {
   return (
     <div className={styles.container}>
+      <h1>Skillset</h1>
+      <div className={styles.sections}>
+        {
+          skillset.map((section, i) =>
+            <Section section={section} key={i} />
+          )
+        }
+      </div>
     </div>
   );
 }
