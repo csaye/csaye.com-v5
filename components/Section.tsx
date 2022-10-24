56f5f7be8627ce729af8ca04a424f0a251ab86ec
Cooper Saye <coopersaye@gmail.com>
2022-10-23 23:55:10 -0400

    Update section component props.

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,4 +1,14 @@
+import { SkillsetSection } from '../data/skillset';
 import styles from '../styles/components/Section.module.scss';
+import Subsection from './Subsection';
+
+type Props = {
+  section: SkillsetSection;
+};
+
+export default function Section(props: Props) {
+  const { section } = props;
+  const { title, subsections } = section;
 
 export default function Section() {
   return (
