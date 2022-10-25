2ca65aaf05b4975dafe4dede5488b6ecca1269d7
Cooper Saye <coopersaye@gmail.com>
2022-10-24 23:57:18 -0400

    Define skillset games section.

--- data/skillset.ts
+++ data/skillset.ts
@@ -7,3 +7,23 @@ export type SkillsetSection = {
   title: string;
   subsections: SkillsetSubsection[];
 }
+
+export const skillset: SkillsetSection[] = [
+  {
+    title: "Games",
+    subsections: [
+      {
+        title: "Frameworks",
+        icons: ["Unity", "Pygame", "MonoGame"]
+      },
+      {
+        title: "Languages",
+        icons: ["Python", "C#", "Java", "C++"]
+      },
+      {
+        title: "Audio",
+        icons: ["FL Studio", "Audacity"]
+      }
+    ]
+  },
+];
