cb5af7e785edd67ace101242ede07e500a378944
Cooper Saye <coopersaye@gmail.com>
2023-04-22 14:18:28 -0400

    Listen for scroll on background component.

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import styles from '../styles/components/Background.module.scss';
 
 export default function Background() {
+  const [scroll, setScroll] = useState(0);
+
+  // set up scroll event listener
+  useEffect(() => {
+    function onScroll() {
+      const newScroll = Math.min(window.scrollY, window.innerHeight);
+      setScroll(newScroll);
+    }
+    addEventListener('scroll', onScroll);
+    return () => removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
     <div className={styles.container}>
+      <div
+        style={{ top: `-${scroll * 0.4 + 64}px` }}
+        className={styles.content}
+      />
     </div>
   );
 }
